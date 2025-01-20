a70ba52b33e693061420ed6c0a02ebbfd7ff8650
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-20 16:12:02 +0800

    npx create-react-app my-app

---
+++ test/my-app/src/App.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { useState } from "react";
+
+function App() {
+  const [state, setState] = useState(0);
+  console.log("App " + state);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setState((count) => count + 1);
+          setState((count) => count * 2);
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
